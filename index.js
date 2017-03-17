89041acb41deff721511122a2fb8b0ab6c711689
Tianxiao <tianxiaom@citrix.com>
2017-03-17 17:14:07 +0800

    generate entities for azure table storage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // load env variables
 require('dotenv').config()
 
+const azure = require('azure-storage')
 const Podio = require('podio-js').api
 const config = require('./config')
 
@@ -10,9 +11,12 @@ const api = new Podio({
   clientSecret: process.env.SECRET
 })
 
-function collectCoursePool (api) {
-  return api.request('GET', `/item/app/${config.course_pool_app_id}`)
-    .then(data => data.items.map(
+const retryOperation = new azure.ExponentialRetryPolicyFilter()
+const tableService = azure.createTableService().withFilter(retryOperation)
+
+function collectCoursePool () {
+  return getPodioAppItems(config.course_pool_app_id)
+    .then(data => data.map(
       item => item.fields.reduce(
         (acc, val) => Object.assign(
           acc,
@@ -22,24 +26,27 @@ function collectCoursePool (api) {
     )
 }
 
-function collectCourseOffer (api) {
+function collectCourseOffer () {
   return Promise.all(config.offer.map(
-    offer => api.request('GET', `/item/app/${offer.app_id}`)
-      .then(data => data.items.map(
+    offer => getPodioAppItems(offer.app_id)
+      .then(data => data.map(
         item => item.fields.reduce(
           (acc, val) => Object.assign(
             acc,
-            { id: item.item_id, Year: offer.year },
+            {
+              id: item.item_id,
+              Year: offer.year
+            },
             { [val.label]: getValue(val) }
           ), {})
       ))
   ))
 }
 
-function collectTrainingPlan (api) {
+function collectTrainingPlan () {
   return Promise.all(config.team.map(
-    team => api.request('GET', `/item/app/${team.app_id}`)
-      .then(data => data.items.map(
+    team => getPodioAppItems(team.app_id)
+      .then(data => data.map(
         item => item.fields
           .filter(field => field.label === 'Attendee Name')
           .map(
@@ -62,8 +69,8 @@ function collectTrainingPlan (api) {
   )
 }
 
-function collectData (api) {
-  return Promise.all([collectCoursePool(api), collectCourseOffer(api), collectTrainingPlan(api)])
+function collectData () {
+  return Promise.all([collectCoursePool(), collectCourseOffer(), collectTrainingPlan()])
     .then(values => {
       const [coursePool, courseOffer, trainingPlan] = values
       return {
@@ -74,8 +81,56 @@ function collectData (api) {
     })
 }
 
-function uploadToAzure (data) {
+function deleteAzureTable (service, tableName) {
+  return new Promise((resolve, reject) =>
+    service.deleteTableIfExists(tableName, (err, response) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(response)
+      }
+    })
+  )
+}
 
+function createAzureTable (service, tableName) {
+  return new Promise((resolve, reject) =>
+    service.createTableIfNotExists(tableName, (err, result, response) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ result, response })
+      }
+    })
+  )
+}
+
+function uploadToAzure (data) {
+  data.map()
+
+  const batch = new azure.TableBatch()
+}
+
+function generateEntities (data, partitionKey, rowKey = 'id') {
+  const entGen = azure.TableUtilities.entityGenerator
+
+  return data.map(d => {
+    const entity = Object.keys(d).reduce((acc, key) => {
+      if (/^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/.test(d[key])) {
+        // suppose this app only used in China office
+        acc[key] = entGen.DateTime(new Date(`${d[key]} GMT+0800`))
+      } else {
+        acc[key] = entGen.String(d[key])
+      }
+
+      return acc
+    }, {})
+
+    return Object.assign(entity, {
+      PartitionKey: entGen.String(d[partitionKey] || partitionKey),
+      RowKey: entGen.String(d[rowKey])
+    })
+  })
 }
 
 function getValue (field) {
@@ -99,8 +154,32 @@ function flatten (arr) {
   ), [])
 }
 
+function getPodioAppItems (appId) {
+  const callApi = (offset = 0) => api.request('GET', `/item/app/${appId}?limit=500&offset=${offset}`)
+  const retriveData = (result = []) => {
+    return callApi(result.length)
+      .then(data => {
+        result = result.concat(data.items)
+
+        if (result.length < data.total) {
+          return retriveData(result)
+        } else {
+          return result
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  return retriveData()
+}
+
 api.authenticateWithCredentials(process.env.USERNAME, process.env.PASSWORD, (err) => {
   if (!err) {
-    collectData(api).then()
+    collectData().then(data => {
+      const {coursePool, courseOffer, trainingPlan} = data
+      console.log(generateEntities(coursePool, 'Course', 'id')[0])
+      console.log(generateEntities(courseOffer, 'Year', 'id')[0])
+      console.log(generateEntities(trainingPlan, 'Team', 'id')[0])
+    })
   }
 })
